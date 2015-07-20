0e97157b26726b4e42d540f4dc6df55ea6ac23cd
Vyacheslav Basenko <gordan.digrizz@gmail.com>
2015-07-20 14:22:37 +0300

    Social services config parameters added

--- config/services.php
+++ config/services.php
@@ -35,4 +35,16 @@ return [
         'secret' => '',
     ],
 
+    'facebook' => [
+        'client_id' => env('FB_CLIENT_ID'),
+        'client_secret' => env('FB_CLIENT_SECRET'),
+        'redirect' => env('SOCIAL_REDIRECT'),
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('SOCIAL_REDIRECT'),
+    ],
+
 ];
