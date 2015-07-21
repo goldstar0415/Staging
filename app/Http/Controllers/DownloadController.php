95ef6f65614a77b6c090a19fe4d263e34496f5a5
Vyacheslav Basenko <gordan.digrizz@gmail.com>
2015-07-21 19:38:27 +0300

    Added download controller for uploaded files

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -12,11 +12,13 @@ class DownloadController extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @param Request $request
      * @return Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $path = storage_path('app/upload/') . $request->get('file');
+        return response()->download($path);
     }
 
 }
