35d08677169db12c57818c492f0525be6c4ef1ac
Vyacheslav Basenko <gordan.digrizz@gmail.com>
2015-07-28 19:04:05 +0300

    Created privacy service resolver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\Privacy;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment() == 'local') {
             $this->app->register(\Laracasts\Generators\GeneratorsServiceProvider::class);
         }
+        $this->app->bind(Privacy::class, function ($app) {
+            return new Privacy($app['request']->user(), $app['auth']);
+        });
     }
 }
