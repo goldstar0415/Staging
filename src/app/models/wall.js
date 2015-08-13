77ba9fe2c304c627253afe87944a47635387f8a0
Serj Korchma <serj.korchma@gmail.com>
2015-08-13 15:34:41 +0300

    add plans

--- src/app/models/wall.js
+++ src/app/models/wall.js
@@ -9,10 +9,12 @@
   function Wall($resource, API_URL) {
     return $resource(API_URL + '/wall/:id', {id: '@id'}, {
       like: {
-        url: API_URL + '/wall/:id/like'
+        url: API_URL + '/wall/:id/like',
+        ignoreLoadingBar: true
       },
       dislike: {
-        url: API_URL + '/wall/:id/like'
+        url: API_URL + '/wall/:id/dislike',
+        ignoreLoadingBar: true
       }
     });
   }
