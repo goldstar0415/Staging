96c9f06fccf1adea964c6d7cd18c65b6fe166d98
Vyacheslav Basenko <gordan.digrizz@gmail.com>
2015-09-04 22:44:55 +0300

    Roles view changed

--- app/Role.php
+++ app/Role.php
@@ -6,8 +6,6 @@ use Zizaco\Entrust\EntrustRole;
 
 class Role extends EntrustRole
 {
-    protected $visible = ['name', 'display_name'];
-
     public static function take($role_name)
     {
         return self::where('name', $role_name)->first();
