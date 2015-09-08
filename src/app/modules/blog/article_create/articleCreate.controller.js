0bc290f0e729088811c9deb5f7866ff3ba4b56cf
Serj Korchma <serj.korchma@gmail.com>
2015-09-08 18:08:46 +0300

    blog post page

--- src/app/modules/blog/article_create/articleCreate.controller.js
+++ src/app/modules/blog/article_create/articleCreate.controller.js
@@ -6,8 +6,35 @@
     .controller('ArticleCreateController', ArticleCreateController);
 
   /** @ngInject */
-  function ArticleCreateController() {
+  function ArticleCreateController(categories, API_URL, UploaderService) {
     var vm = this;
+    vm.categories = categories;
+    vm.images = UploaderService.images;
 
+    vm.save = save;
+
+
+    function save(form) {
+      if (form.$valid) {
+        var req = {},
+          url = API_URL + '/posts';
+
+        req.payload = JSON.stringify(vm);
+        if (vm.id) {
+          req._method = 'PUT';
+          url = API_URL + '/posts/' + vm.id;
+        }
+
+        UploaderService
+          .upload(url, req)
+          .then(function (resp) {
+
+            //$state.go('spot', {spot_id: resp.data.id, user_id: resp.data.user_id});
+          })
+          .catch(function (resp) {
+            toastr.error('Upload failed');
+          });
+      }
+    }
   }
 })();
