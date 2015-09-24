9b39400b2474137f70f8628da4e2e0023b3f019d
Vyacheslav Basenko <gordan.digrizz@gmail.com>
2015-09-24 13:22:36 +0300

    Fixed serialization of OnMessage Event

--- app/Listeners/MailNotifier.php
+++ app/Listeners/MailNotifier.php
@@ -42,6 +42,7 @@ class MailNotifier
     {
         switch (true) {
             case $event instanceof OnMessage:
+                $event = unserialize(serialize($event));//TODO: remove when fix
                 $this->send($event->message->receiver, 'message', [
                     'sender' => $event->message->sender()->first(),
                     'receiver' => $event->message->receiver()->first()
