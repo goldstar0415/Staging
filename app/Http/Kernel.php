846778b042325d4ebc09bba115268d8cef55d5c9
Vyacheslav Basenko <gordan.digrizz@gmail.com>
2015-09-29 16:52:40 +0300

    Online property last_action_at added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \Barryvdh\Cors\HandleCors::class,
         \App\Http\Middleware\JsonPayloadParse::class,
+        \App\Http\Middleware\CheckOnline::class,
 //      \App\Http\Middleware\VerifyCsrfToken::class, TODO: uncomment after development
     ];
 
