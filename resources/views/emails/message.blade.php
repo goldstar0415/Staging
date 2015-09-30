0d50212daaeae9c7f8f6c9a93b2eb74d848b2566
Vyacheslav Basenko <gordan.digrizz@gmail.com>
2015-09-30 14:01:18 +0300

    Notifications changes

--- resources/views/emails/message.blade.php
+++ resources/views/emails/message.blade.php
@@ -1,2 +1,2 @@
 You have new message on <a href="{{ frontend_url() }}">Zoomtivity</a> from {{ $sender->first_name }} {{ $sender->last_name }}.
-<a href="{{ frontend_url('user', $receiver->id) }}">See message</a>
+<a href="{{ frontend_url('chat', $sender->id) }}">See message</a>
