1041c4ba9bdb8f811dd6aef9c45ee548fa56c2e3
Serj Korchma <serj.korchma@gmail.com>
2015-11-10 12:59:23 +0200

    convert user photos to base64

--- src/app/components/google_contacts/google_contacts.controller.js
+++ src/app/components/google_contacts/google_contacts.controller.js
@@ -41,18 +41,21 @@
       });
     };
 
-    function convertToBase64(url, callback) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'blob';
-      xhr.onload = function() {
-        var reader  = new FileReader();
-        reader.onloadend = function () {
-          callback(reader.result);
-        };
-        reader.readAsDataURL(xhr.response);
+    function convertToBase64(url, callback, outputFormat) {
+      var img = new Image();
+      img.crossOrigin = 'Anonymous';
+      img.onload = function(){
+        var canvas = document.createElement('CANVAS');
+        var ctx = canvas.getContext('2d');
+        var dataURL;
+        canvas.height = this.height;
+        canvas.width = this.width;
+        ctx.drawImage(this, 0, 0);
+        dataURL = canvas.toDataURL(outputFormat);
+        callback(dataURL);
+        canvas = null;
       };
-      xhr.open('GET', url);
-      xhr.send();
+      img.src = url;
     }
 
 
