4c4033d6aa2d62dc096f0372dbc976c498e30db1
Serj Korchma <serj.korchma@gmail.com>
2015-11-17 18:27:48 +0200

    add autocomplete to location and users

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
     <title>ZoomTivity</title>
+
     <link rel="stylesheet" href="/css/admin.css" />
 </head>
 
@@ -30,6 +31,17 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="/js/bootstrap.min.js"></script>
+
+<!--SELECT2-->
+<link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1-rc.1/css/select2.min.css" rel="stylesheet" />
+<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1-rc.1/js/select2.min.js"></script>
+
+<!--CKeditor-->
+<script src="//cdn.ckeditor.com/4.5.5/standard/ckeditor.js"></script>
+
+<!--Others-->
+<script src="/js/admin.js"></script>
+
 </body>
 
 
