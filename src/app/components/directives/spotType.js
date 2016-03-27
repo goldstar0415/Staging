128f695f25679a75574864e6e28c70c154170921
Serj Korchma <serj.korchma@gmail.com>
2016-03-27 17:17:53 +0300

    fix #122

--- src/app/components/directives/spotType.js
+++ src/app/components/directives/spotType.js
@@ -8,24 +8,26 @@
         restrict: 'EA',
         replace: true,
         scope: {
-          name: '=type'
+          name: '=type',
+          class: '='
         },
-        template: '<span class="spot-type {{ spotTypeClass }}">{{ spotTypeName }}</span>"',
+        template: '<span ng-class="spotTypeClass">{{ spotTypeName }}</span>"',
         link: function (s, e, a) {
           s.spotTypeName = s.name;
+          s.spotTypeClass = s.class || 'spot-type';
 
           switch (s.name) {
             case 'Event':
-              s.spotTypeClass = 'event-spot-page';
+              s.spotTypeClass += ' event-spot-page';
               break;
             case 'To-Do':
-              s.spotTypeClass = 'recreation-spot';
+              s.spotTypeClass += ' recreation-spot';
               break;
             case 'Food':
-              s.spotTypeClass = 'pitstop-spot';
+              s.spotTypeClass += ' pitstop-spot';
               break;
             case 'Shelter':
-              s.spotTypeClass = 'recreation-spot';
+              s.spotTypeClass += ' recreation-spot';
               break;
           }
         }
