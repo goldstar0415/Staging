747d99b71aa90ed2e8fde949a3f987a41d6c19af
Serj Korchma <serj.korchma@gmail.com>
2016-04-08 18:38:24 +0300

    fix #52

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -538,9 +538,8 @@
         template: '<ui-view  />',
         abstract: true,
         resolve: {
-          user: function ($rootScope, User, currentUser, $stateParams, UserService, $state) {
-
-            if (currentUser && currentUser.alias == $stateParams.user_id) {
+          user: function ($rootScope, User, currentUser, $stateParams, UserService) {
+            if (currentUser && (currentUser.id == $stateParams.user_id || currentUser.alias == $stateParams.user_id)) {
               return User.currentUser({}, function (user) {
                 $rootScope.currentUser = user;
                 UserService.setProfileUser(user);
