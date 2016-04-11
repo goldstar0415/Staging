0b14437c91e292553ef3c132364a7534390e8ce5
Serj Korchma <serj.korchma@gmail.com>
2016-04-11 03:21:14 +0300

    fix dependencies

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,26 +1,21 @@
-(function () {
-  'use strict';
-
-  angular
-    .module('zoomtivity', [
-      'ngAnimate',
-      //'ngCookies',
-      //'ngTouch',
-      'ngSanitize',
-      'ngResource',
-      'ngMessages',
-      'ngFileUpload',
-      'ui.router',
-      'ui.bootstrap',
-      'dialogs.main',
-      'snap',
-      'angular-loading-bar',
-      'luegg.directives',
-      'ui.calendar',
-      'ngTagsInput',
-      'ui.select',
-      'ui.utils.masks',
-      'infinite-scroll'
-    ]);
-
-})();
+angular
+  .module('zoomtivity', [
+    'ngAnimate',
+    //'ngCookies',
+    //'ngTouch',
+    'ngSanitize',
+    'ngResource',
+    'ngMessages',
+    'ngFileUpload',
+    'ui.router',
+    'ui.bootstrap',
+    'dialogs.main',
+    'snap',
+    'angular-loading-bar',
+    'luegg.directives',
+    'ui.calendar',
+    'ngTagsInput',
+    'ui.select',
+    'ui.utils.masks',
+    'infinite-scroll'
+  ]);
