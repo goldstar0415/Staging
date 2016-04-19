bf101a2cd13dd5e074054cb0b1f4b167b6357f5b
Oleg Yurevich <oleg.yurevich@omnigon.com>
2016-04-19 20:30:21 +0300

    ZDEBUG-368 Back to "My Blog" after  blog saving

--- src/app/modules/blog/article_create/articleCreate.controller.js
+++ src/app/modules/blog/article_create/articleCreate.controller.js
@@ -1,61 +1,61 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('zoomtivity')
-    .controller('ArticleCreateController', ArticleCreateController);
+    angular
+        .module('zoomtivity')
+        .controller('ArticleCreateController', ArticleCreateController);
 
-  /** @ngInject */
-  function ArticleCreateController($state, article, categories, toastr, API_URL, UploaderService) {
-    var vm = this;
-    vm = _.extend(vm, article);
-    vm.categories = categories;
-    vm.images = UploaderService.images;
+    /** @ngInject */
+    function ArticleCreateController($state, article, categories, toastr, API_URL, UploaderService) {
+        var vm = this;
+        vm = _.extend(vm, article);
+        vm.categories = categories;
+        vm.images = UploaderService.images;
 
-    vm.save = save;
+        vm.save = save;
 
-    if(!vm.body) {
-      vm.body = '<p></p><p></p><p></p><p></p><p></p><p></p>';
-    }
-
-    /*
-     * Create post
-     * @param form {ngForm}
-     */
-    function save(form) {
-      var data = angular.copy(vm),
-        req = {},
-        url = API_URL + '/posts';
-
-      if (form.$valid) {
-        delete data.categories;
-        delete data.images;
-
-        //get wysiwyg content
-        var editor = ContentTools.EditorApp.get();
-        editor.save();
-        data.body = angular.element('[content-tools]').html();
-
-        req.payload = JSON.stringify(data);
-        if (vm.id) {
-          req._method = 'PUT';
-          url = API_URL + '/posts/' + vm.slug;
+        if (!vm.body) {
+            vm.body = '<p></p><p></p><p></p><p></p><p></p><p></p>';
         }
 
-        vm.images.files.splice(0, vm.images.files.length - 1);  //save last image
-        UploaderService
-          .upload(url, req, 'cover')
-          .then(function (resp) {
-            $state.go('blog.article', {slug: resp.data.slug});
-          })
-          .catch(function (resp) {
-            _.each(resp.data, function (message) {
-              toastr.error(_.isArray(message) ? message[0] : message);
-            });
+        /*
+         * Create post
+         * @param form {ngForm}
+         */
+        function save(form) {
+            var data = angular.copy(vm),
+                req = {},
+                url = API_URL + '/posts';
 
-            editor.start();
-          });
-      }
+            if (form.$valid) {
+                delete data.categories;
+                delete data.images;
+
+                //get wysiwyg content
+                var editor = ContentTools.EditorApp.get();
+                editor.save();
+                data.body = angular.element('[content-tools]').html();
+
+                req.payload = JSON.stringify(data);
+                if (vm.id) {
+                    req._method = 'PUT';
+                    url = API_URL + '/posts/' + vm.slug;
+                }
+
+                vm.images.files.splice(0, vm.images.files.length - 1);  //save last image
+                UploaderService
+                    .upload(url, req, 'cover')
+                    .then(function (resp) {
+                        $state.go('profile_blog', {user_id: vm.user_id});
+                    })
+                    .catch(function (resp) {
+                        _.each(resp.data, function (message) {
+                            toastr.error(_.isArray(message) ? message[0] : message);
+                        });
+
+                        editor.start();
+                    });
+            }
+        }
     }
-  }
 })();
