99b61d1b9a539177732c8d489386128fa975ec20
Ingvar <igor@wirnex.com>
2016-06-02 01:09:13 +0300

    materialized view, todo filters and icon_url

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -7,11 +7,14 @@ use App\Http\Requests\Map\SpotListRequest;
 use App\Http\Requests\Map\SpotsSearchRequest;
 use App\Http\Requests\WeatherRequest;
 use App\Spot;
+use App\SpotView;
 use App\SpotPoint;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Nwidart\ForecastPhp\Forecast;
-
+use Log;
+use DB;
+use App\Http\Controllers\Event;
 use App\Http\Requests;
 
 /**
@@ -57,68 +60,60 @@ class MapController extends Controller
         /**
          * @var $spots \Illuminate\Database\Query\Builder
          */
-        $spots = Spot::select('spots.*')->where('is_private', false);
-
-        if ($request->has('search_text')) {
+        $spots = SpotView::select(
+			'mv_spots_spot_points.*',
+			DB::raw("split_part(trim(ST_AsText(mv_spots_spot_points.location)::text, 'POINT()'), ' ', 2)::float AS lat"),
+			DB::raw("split_part(trim(ST_AsText(mv_spots_spot_points.location)::text, 'POINT()'), ' ', 1)::float AS lng")
+		)->where('is_private', false);
+		/** @todo search text */
+        /*if ($request->has('search_text')) {
             $spots->where(function ($query) use ($request) {
                 $query->where('spots.title', 'ilike', "%$request->search_text%")
                     ->orWhereRaw('spots.id in (select spot_id from spot_points where address ilike ?)', ["%$request->search_text%"]);
             });
         }
-
-        if ($request->has('filter.category_ids')) {
+		/** @todo catgory ids */
+        /*if ($request->has('filter.category_ids')) {
             $spots->join('spot_type_categories', 'spot_type_categories.id', '=', 'spots.spot_type_category_id')
                 ->whereIn('spot_type_categories.id', $request->filter['category_ids']);
-        }
+        }*/
 
         if ($request->has('type')) {
-			try {
-				// pick hardcoded type id
-				$spots->where('spots.spot_type_category_id', \App\SpotType::getTypeId($request->type));
-			} catch (Exception $ex) {
-				if (!$request->has('filter.category_ids')) {
-					$spots->join('spot_type_categories', 'spot_type_categories.id', '=', 'spots.spot_type_category_id');
-				}
-				$spots->join('spot_types', 'spot_type_categories.spot_type_id', '=', 'spot_types.id')
-					->where('spot_types.name', $request->type);
-			}
+			$spots->whereRaw("spot_type_category_id in (
+				select id from spot_type_categories where spot_type_id in (select id from spot_types WHERE name = '{$request->type}'))");
         }
 
-        if ($request->has('filter.start_date')) {
-            $spots->where(function ($query) use ($request) {
-                $query->where('start_date', '>=', $request->filter['start_date'])->orWhereNull('start_date');
-            });
-        } else {
-            $spots->where(function ($query) use ($request) {
-                $query->where('start_date', '>=', Carbon::now()->format('Y-m-d'))->orWhereNull('start_date');
-            });
-        }
+		if ($request->has('filter.start_date')) {
+			$spots->where('start_date', '>=', $request->filter['start_date']);
+		} else {
+			if ($request->has('type') && in_array($request->type, ['event'])) {
+				$spots->where('start_date', '>=', Carbon::now()->format('Y-m-d'));
+			}
+		}
 
         if ($request->has('filter.end_date')) {
-            $spots->where(function ($query) use ($request) {
-                $query->where('end_date', '<=', $request->filter['end_date'] . ' 23:59:59')->orWhereNull('end_date');
-            });
+            $spots->where('end_date', '<=', $request->filter['end_date'] . ' 23:59:59');
         }
-
-        if ($request->has('filter.tags')) {
+		/** @todo tags */
+        /*if ($request->has('filter.tags')) {
             $spots->joinWhere('tags', 'tags.name', 'in', $request->filter['tags']);
             $spots->join('spot_tag', function ($join) {
                 $join->on('spot_tag.spot_id', '=', 'spots.id')->on('spot_tag.tag_id', '=', 'tags.id');
             });
-        }
-
-        if ($request->has('filter.rating')) {
+        }*/
+		/** @todo rating */
+        /*if ($request->has('filter.rating')) {
             $spots->addSelect('spot_votes.vote');
             $spots->join('spot_votes', 'spot_votes.spot_id', '=', 'spots.id');
             $spots->groupBy('spots.id', 'vote')->havingRaw('avg(vote) > ' . $request->filter['rating']);
-        }
+        }*/
 
         if ($request->has('filter.b_boxes')) {
             if (!$request->has('search_text')) {
 				$search_areas = [];
 				foreach ($request->filter['b_boxes'] as $b_box) {
 					$search_areas[] = sprintf(
-						'spots.id in (select spot_id from spot_points where "location" && ST_MakeEnvelope(%s, %s, %s, %s, 4326))',
+						'"mv_spots_spot_points"."location" && ST_MakeEnvelope(%s, %s, %s, %s, 4326)',
 						$b_box['_southWest']['lng'],
 						$b_box['_southWest']['lat'],
 						$b_box['_northEast']['lng'],
@@ -128,15 +123,41 @@ class MapController extends Controller
 				$spots->whereRaw(implode(' OR ', $search_areas));
 			}
         }
-		
-        $points = [];
-        $spots->skip(0)->take(1000)->get()->each(function ($spot) use (&$points) {
-            $points = array_merge($points, $spot->points->map(function (SpotPoint $point) use ($spot) {
-                $point->setAttribute('category_icon_url', $spot->category->icon_url);
-                return $point;
-            })->all());
-        });
+		// Display all SQL executed in Eloquent
 
+		/** @todo icon_url */
+        $points = [];
+		//Log::debug($spots);
+		Log::debug($spots->skip(0)->take(1000)->toSql());
+		$iconUrlCache = [];
+		$tt = microtime(1);
+        $spotsArr = $spots->skip(0)->take(1000)->get();
+		foreach($spotsArr as $spot) {
+			$points[] = [
+				'id'		=> $spot->spot_point_id,
+				'spot_id'	=> $spot->id,
+				'location' => [
+					'lat' => $spot->lat,
+					'lng' => $spot->lng
+				],
+				'category_icon_url' => "http://zoomtivity.wirnex.com/uploads/missings/icons/original/missing.png",
+				//'address'			=> $spot->address
+			];
+		}
+				//each(function ($spot) use (&$points, &$iconUrlCache) {
+			//Log::debug((array)$spot);
+            //$points = array_merge($points, $spot->points->map(function (SpotPoint $point) use ($spot, &$iconUrlCache) {
+				/*if (!array_key_exists($spot->spot_type_category_id, $iconUrlCache)) {
+					$iconUrlCache[$spot->spot_type_category_id] = $spot->category->icon_url;
+					Log::debug('cache miss '.$spot->spot_type_category_id);
+				}
+                $point->setAttribute('category_icon_url', $iconUrlCache[$spot->spot_type_category_id]);*/
+			//	unset($point->address);
+//                return $point;
+  //          })->all());
+        //});
+		Log::debug(microtime(1) - $tt);
+		Log::debug("");
 		return $points;
     }
 
---
+++ app/SpotView.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App;
+
+use App\Contracts\CalendarExportable;
+use App\Contracts\Commentable;
+use App\Extensions\Attachable;
+use App\Extensions\StartEndDatesTrait;
+use App\Scopes\ApprovedScopeTrait;
+use App\Scopes\NewestScopeTrait;
+use App\Services\SocialSharing;
+use Codesleeve\Stapler\ORM\EloquentTrait as StaplerTrait;
+use Codesleeve\Stapler\ORM\StaplerableInterface;
+use DB;
+use Eluceo\iCal\Component\Event;
+use Eluceo\iCal\Property\Event\Organizer;
+use Request;
+use Log;
+
+/**
+ * Class Spot
+ * @package App
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property integer $spot_type_category_id
+ * @property string $title
+ * @property string $description
+ * @property array $web_sites
+ * @property \Codesleeve\Stapler\Attachment $cover
+ * @property array $videos
+ * @property bool $is_approved
+ * @property bool $is_private
+ * @property \Carbon\Carbon $start_date
+ * @property \Carbon\Carbon $end_date
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ *
+ * Relation properties
+ * @property User $user
+ * @property SpotTypeCategory $category
+ * @property \Illuminate\Database\Eloquent\Collection $walls
+ * @property \Illuminate\Database\Eloquent\Collection $votes
+ * @property \Illuminate\Database\Eloquent\Collection $comments
+ * @property \Illuminate\Database\Eloquent\Collection $favorites
+ * @property \Illuminate\Database\Eloquent\Collection $tags
+ * @property \Illuminate\Database\Eloquent\Collection $plans
+ * @property \Illuminate\Database\Eloquent\Collection $points
+ * @property \Illuminate\Database\Eloquent\Collection $calendarUsers
+ *
+ * Mutators properties
+ * @property float $rating
+ * @property array $locations
+ * @property string $type
+ * @property \Illuminate\Database\Eloquent\Collection $comments_photos
+ */
+class SpotView extends BaseModel
+{
+	protected $table = 'mv_spots_spot_points';
+	public $timestamps = false;
+	/**
+     * Get the points for the spot
+     */
+    public function points()
+    {
+        return $this->hasMany(SpotPoint::class, 'spot_id');
+    }
+}
