07f1e55ec147b67aa3514a0b6a87c21b99c2a48e
Igor Prokofev <prokofevigor@mail.ru>
2016-08-31 14:20:33 +0300

    js5 service-worker

--- src/assets/libs/ServiceWorker/service-worker.js
+++ src/assets/libs/ServiceWorker/service-worker.js
@@ -1,62 +1,52 @@
 
 'use strict';
 
-const CACHE_VERSION = 1;
-let CURRENT_CACHES = {
+var CACHE_VERSION = 1;
+var CURRENT_CACHES = {
   offline: 'offline-v' + CACHE_VERSION
 };
-const OFFLINE_URL = 'offline.html';
+var OFFLINE_URL = 'offline.html';
 
 function createCacheBustedRequest(url) {
-  let request = new Request(url, {cache: 'reload'});
+  var request = new Request(url, { cache: 'reload' });
   if ('cache' in request) {
     return request;
   }
 
-  let bustedUrl = new URL(url, self.location.href);
+  var bustedUrl = new URL(url, self.location.href);
   bustedUrl.search += (bustedUrl.search ? '&' : '') + 'cachebust=' + Date.now();
   return new Request(bustedUrl);
 }
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    fetch(createCacheBustedRequest(OFFLINE_URL)).then(function(response) {
-      return caches.open(CURRENT_CACHES.offline).then(function(cache) {
-        return cache.put(OFFLINE_URL, response);
-      });
-    })
-  );
+self.addEventListener('install', function (event) {
+  event.waitUntil(fetch(createCacheBustedRequest(OFFLINE_URL)).then(function (response) {
+    return caches.open(CURRENT_CACHES.offline).then(function (cache) {
+      return cache.put(OFFLINE_URL, response);
+    });
+  }));
 });
 
-self.addEventListener('activate', event => {
-  let expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+self.addEventListener('activate', function (event) {
+  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function (key) {
     return CURRENT_CACHES[key];
   });
 
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (expectedCacheNames.indexOf(cacheName) === -1) {
-            console.log('Deleting out of date cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+  event.waitUntil(caches.keys().then(function (cacheNames) {
+    return Promise.all(cacheNames.map(function (cacheName) {
+      if (expectedCacheNames.indexOf(cacheName) === -1) {
+        console.log('Deleting out of date cache:', cacheName);
+        return caches.delete(cacheName);
+      }
+    }));
+  }));
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate' ||
-      (event.request.method === 'GET' &&
-       event.request.headers.get('accept').includes('text/html'))) {
+self.addEventListener('fetch', function (event) {
+  if (event.request.mode === 'navigate' || event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html')) {
     console.log('Handling fetch event for', event.request.url);
-    event.respondWith(
-      fetch(createCacheBustedRequest(event.request.url)).catch(error => {
-        console.log('Fetch failed; returning offline page instead.', error);
-        return caches.match(OFFLINE_URL);
-      })
-    );
+    event.respondWith(fetch(createCacheBustedRequest(event.request.url)).catch(function (error) {
+      console.log('Fetch failed; returning offline page instead.', error);
+      return caches.match(OFFLINE_URL);
+    }));
   }
 });
