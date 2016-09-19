6a337875ff4eebd2419c504cb78b6c027bc51b50
Eugene Bondar <j.bondar@2-up.ru>
2016-09-19 12:08:28 +0300

    blog fixes

--- src/app/modules/blog/article_create/articleCreate.controller.js
+++ src/app/modules/blog/article_create/articleCreate.controller.js
@@ -6,11 +6,14 @@
         .controller('ArticleCreateController', ArticleCreateController);
 
     /** @ngInject */
-    function ArticleCreateController($state, article, categories, toastr, API_URL, UploaderService) {
+    function ArticleCreateController($state, article, categories, toastr, API_URL, UploaderService, $rootScope) {
         var vm = this;
         vm = _.extend(vm, article);
         vm.categories = categories;
         vm.images = UploaderService.images;
+        vm.options = {
+            height: 200
+        };
 
         vm.save = save;
 
@@ -30,30 +33,30 @@
             if (form.$valid) {
                 delete data.categories;
                 delete data.images;
-
+                delete data.options;
+                
                 //get wysiwyg content
-                var editor = ContentTools.EditorApp.get();
-                editor.save();
-                data.body = angular.element('[content-tools]').html();
-
+                //var editor = ContentTools.EditorApp.get();
+                //editor.save();
+                //data.body = angular.element('[content-tools]').html();
                 req.payload = JSON.stringify(data);
                 if (vm.id) {
                     req._method = 'PUT';
-                    url = API_URL + '/posts/' + vm.slug;
+                    url = API_URL + '/posts/' + vm.id;
                 }
 
                 vm.images.files.splice(0, vm.images.files.length - 1);  //save last image
                 UploaderService
                     .upload(url, req, 'cover')
                     .then(function (resp) {
-                        $state.go('profile_blog', {user_id: vm.user_id});
+                        $state.go('profile_blog', {user_id: vm.user_id || $rootScope.currentUser.id });
                     })
                     .catch(function (resp) {
                         _.each(resp.data, function (message) {
                             toastr.error(_.isArray(message) ? message[0] : message);
                         });
 
-                        editor.start();
+                        //editor.start();
                     });
             }
         }
