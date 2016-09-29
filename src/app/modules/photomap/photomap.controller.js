61d79bc8501e0c780b9cad83ef427977d9600db4
Gleb <Gleb>
2016-09-29 11:35:12 +0300

    Photo Map stack effect added

--- src/app/modules/photomap/photomap.controller.js
+++ src/app/modules/photomap/photomap.controller.js
@@ -10,8 +10,50 @@
     var vm = this;
     vm.currentId = $stateParams.user_id;
     vm.albums = albums;
+    vm.getRandomClass = getRandomClass;
+    vm.flexGrid = flexGrid;
+
+    function getRandomClass() {
+        var val = Math.floor((Math.random() * 3) + 1);
+        if (val === 1) {
+            return 'stackone';
+        } else if (val === 2) {
+            return 'stacktwo';
+        } else if (val === 3) {
+            return 'stackthree';
+        }
+    }
 
     initMap();
+
+    function flexGrid() {
+        function checkCount(current_total, next) {
+            if (current_total % 12) {
+                current_total += 1;
+                checkCount(current_total, next);
+            } else {
+                next(current_total);
+            }
+        }
+
+        function addDummyElementsToItems(container) {
+            var card_count = container.children.length;
+            checkCount(card_count, function(final_count) {
+                var dummy_element;
+                for (var i = 0; i < (final_count - card_count); i++) {
+                    dummy_element = document.createElement('div');
+                    dummy_element.style.width = '240px';
+                    // dummy_element.className = 'item-fake';
+                    container.appendChild(dummy_element);
+                }
+            });
+        }
+
+        var grids = document.querySelectorAll('.album-images');
+        grids.forEach(function(item) {
+            addDummyElementsToItems(item);
+        });
+    }
 
     //create marker on map
     function createMarker(iconUrl, title, location, data) {
