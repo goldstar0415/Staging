48c8584b98ea6498f8cf939074992cd385d86353
Gleb <Gleb>
2016-10-12 16:46:43 +0300

    Mobile UI changes

--- src/app/components/navigation/header/header.directive.js
+++ src/app/components/navigation/header/header.directive.js
@@ -1,40 +1,47 @@
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  /*
-   * Directive for header
-   */
+    /*
+     * Directive for header
+     */
 
-  angular
-    .module('zoomtivity')
-    .directive('zmHeader', ZoomtivityHeader);
+    angular
+        .module('zoomtivity')
+        .directive('zmHeader', ZoomtivityHeader);
 
-  /** @ngInject */
-  function ZoomtivityHeader() {
-    return {
-      restrict: 'E',
-      templateUrl: '/app/components/navigation/header/header.html',
-      scope: {
-        options: "="
-      },
-      controller: HeaderController,
-      controllerAs: 'Header',
-      bindToController: true
-    };
-  }
-
-  /** @ngInject */
-  function HeaderController($state, BACKEND_URL) {
-    var vm = this;
-    vm.$state = $state;
-    vm.BACKEND_URL = BACKEND_URL;
-
-    if (vm.options.snap.disable == "left") {
-      vm.toggle = "right";
-    } else {
-      vm.toggle = "left";
+    /** @ngInject */
+    function ZoomtivityHeader() {
+        return {
+            restrict: 'E',
+            templateUrl: '/app/components/navigation/header/header.html',
+            scope: {
+                options: "="
+            },
+            controller: HeaderController,
+            controllerAs: 'Header',
+            bindToController: true
+        };
     }
 
-  }
+    /** @ngInject */
+    function HeaderController($state, BACKEND_URL) {
+        var vm = this;
+        vm.$state = $state;
+        vm.BACKEND_URL = BACKEND_URL;
+        vm.searchClick = searchClick;
+        vm.isSearchOpened = false;
+        vm.searchValue = '';
+
+        if (vm.options.snap.disable == "left") {
+            vm.toggle = "right";
+        } else {
+            vm.toggle = "left";
+        }
+
+        function searchClick() {
+            vm.isSearchOpened = !vm.isSearchOpened;
+            vm.searchValue = '';
+        }
+    }
 
 })();
