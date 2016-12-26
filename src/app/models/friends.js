3349fff056430b1526546c303275257748fc157f
goldstar0415 <topdeveloper1212@gmail.com>
2016-12-26 16:50:10 +0300

    a merge conflict has been resolved, bug-fixes

---
+++ src/app/models/friends.js
@@ -0,0 +1,51 @@
+(function () {
+  'use strict';
+
+  /*
+   * Friend model
+   */
+  angular
+    .module('zoomtivity')
+    .factory('Friends', Friends);
+
+  /** @ngInject */
+  function Friends($resource, API_URL) {
+    return $resource(API_URL + '/friends', {id: '@id'}, {
+      update: {
+        url: API_URL + '/friends/:id',
+        transformRequest: function (data, headers) {
+          var params = {
+            first_name: data.first_name,
+            last_name: data.last_name,
+            birth_date: data.birth_date,
+            phone: data.phone,
+            email: data.email,
+            location: data.location,
+            address: data.address,
+            note: data.note
+          };
+          return JSON.stringify(params);
+        },
+        method: 'PUT'
+      },
+      getFriend: {
+        url: API_URL + '/friends/:id',
+        method: "GET"
+      },
+      deleteFriend: {
+        url: API_URL + '/friends/:id',
+        method: "DELETE"
+      },
+      setAvatar: {
+        url: API_URL + '/friends/:id/setavatar',
+        method: "POST"
+      },
+      followFacebook: {
+        url: API_URL + '/followings/:id/followFacebook',
+        method: 'POST',
+        data: {ids: '@ids'}
+      }
+    });
+  }
+
+})();
