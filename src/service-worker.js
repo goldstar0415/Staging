1681330dd8a4333ce972118e12679860011a9a97
goldstar0415 <topdeveloper1212@gmail.com>
2017-01-25 18:49:49 +0300

    servicer-worker - cache static files (unstable)

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,44 +1,57 @@
-
 'use strict';
 
 var CACHE_VERSION = 1;
 var CURRENT_CACHES = {
-  offline: 'offline-v' + CACHE_VERSION
+    offline: 'offline-v' + CACHE_VERSION,
+    online: 'online-v' + CACHE_VERSION,
 };
 var OFFLINE_URL = 'assets/offline.html';
 
-function createCacheBustedRequest(url) {
-  var request = new Request(url, { cache: 'reload' });
-  if ('cache' in request) {
-    return request;
-  }
+function cachePolicy(req) {
+    return (
+      // Cloudflare CDN files
+      ( /cdnjs\.cloudflare\.com.+\.(js|css)$/i.test(req.url) ) ||
+      // Google Maps files
+      // ( /maps\.googleapis\.com\/maps\/api\/js/i.test(req.url) && req.url.indexOf('AuthenticationService.Authenticate') == -1 ) ||
+      // application files
+      ( 0 === req.url.indexOf(req.referrer) ) ||
+      // facebook SDK
+      ( req.url == 'https://connect.facebook.net/en_US/sdk.js' )
+    );
+}
 
-  var bustedUrl = new URL(url, self.location.href);
-  bustedUrl.search += (bustedUrl.search ? '&' : '') + 'cachebust=' + Date.now();
-  return new Request(bustedUrl);
+function createCacheBustedRequest(url) {
+    var request = new Request(url, {cache: 'reload'});
+    if ('cache' in request) {
+        return request;
+    }
+
+    var bustedUrl = new URL(url, self.location.href);
+    bustedUrl.search += (bustedUrl.search ? '&' : '') + 'cachebust=' + Date.now();
+    return new Request(bustedUrl);
 }
 
 self.addEventListener('install', function (event) {
-  event.waitUntil(fetch(createCacheBustedRequest(OFFLINE_URL)).then(function (response) {
-    return caches.open(CURRENT_CACHES.offline).then(function (cache) {
-      return cache.put(OFFLINE_URL, response);
-    });
-  }));
+    event.waitUntil(fetch(createCacheBustedRequest(OFFLINE_URL)).then(function (response) {
+        return caches.open(CURRENT_CACHES.offline).then(function (cache) {
+            return cache.put(OFFLINE_URL, response);
+        });
+    }));
 });
 
 self.addEventListener('activate', function (event) {
-  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function (key) {
-    return CURRENT_CACHES[key];
-  });
+    var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function (key) {
+        return CURRENT_CACHES[key];
+    });
 
-  event.waitUntil(caches.keys().then(function (cacheNames) {
-    return Promise.all(cacheNames.map(function (cacheName) {
-      if (expectedCacheNames.indexOf(cacheName) === -1) {
-        console.log('Deleting out of date cache:', cacheName);
-        return caches.delete(cacheName);
-      }
+    event.waitUntil(caches.keys().then(function (cacheNames) {
+        return Promise.all(cacheNames.map(function (cacheName) {
+            if (expectedCacheNames.indexOf(cacheName) === -1) {
+                console.log('Deleting out of date cache:', cacheName);
+                return caches.delete(cacheName);
+            }
+        }));
     }));
-  }));
 });
 
 self.addEventListener('fetch', function (event) {
@@ -49,4 +62,37 @@ self.addEventListener('fetch', function (event) {
       return caches.match(OFFLINE_URL);
     }));
   }
+});
+
+self.addEventListener('fetch', function(event) {
+    if (event.request.mode === 'navigate' || event.request.method === 'GET' && cachePolicy(event.request)) {
+        event.respondWith(
+          caches.match(event.request)
+            .then(function (response) {
+
+                if (response) {
+                    console.debug('>>> Hit: ', event.request.url);
+                    return response;
+                }
+
+                var fetchRequest = event.request.clone();
+                console.debug('>>> Requesting: ' + event.request.url);
+                return fetch(fetchRequest).then(
+                  function (response) {
+                      if (!response || response.status !== 200 || ['basic', 'opaque'].indexOf(response.type) == -1) {
+                          console.warn('>>> Do not cache: ' + event.request.url, response);
+                          return response;
+                      }
+                      var responseToCache = response.clone();
+                      caches.open(CURRENT_CACHES.online)
+                        .then(function (cache) {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                      return response;
+                  }
+                );
+            })
+        );
+    }
 });
