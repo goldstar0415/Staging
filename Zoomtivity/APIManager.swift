bf46fb382309c1427a26213d89bf8e509caaa2b2
Callum Trounce <trounce@d-tt.nl>
2017-02-17 21:31:48 +0000

    API Manager class

---
+++ Zoomtivity/APIManager.swift
@@ -0,0 +1,15 @@
+//
+//  APIManager.swift
+//  Zoomtivity
+//
+//  Created by Callum Trounce on 17/02/2017.
+//  Copyright © 2017 Zoomtivity. All rights reserved.
+//
+
+import Foundation
+
+class APIManager : NSObject {
+    
+    static let sharedAPIManager = APIManager()
+    
+}
