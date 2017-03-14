85dfceb8bd2cd8901ff657f873f832fcab8d9b24
Jett <whackashoe@gmail.com>
2017-03-14 18:15:07 -0500

    move external services api keys into config/services

--- backend/config/google.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'places' => [
-        'key' => env('GOOGLE_PLACES_API_KEY', 'AIzaSyCgASDG4tB4A2TqjCxj_MH5pF6LrkDskzY'),
-        'baseUri' => 'https://maps.googleapis.com/maps/api/place/'
-    ],
-];
