7e0fdcfcbc116850ecfd8c312668759937d55023
Jett <whackashoe@gmail.com>
2017-03-14 18:15:07 -0500

    move external services api keys into config/services

--- backend/config/services.php
+++ backend/config/services.php
@@ -14,27 +14,6 @@ return [
     |
     */
 
-    'mailgun' => [
-        'domain' => '',
-        'secret' => '',
-    ],
-
-    'mandrill' => [
-        'secret' => '',
-    ],
-
-    'ses' => [
-        'key' => '',
-        'secret' => '',
-        'region' => 'us-east-1',
-    ],
-
-    'stripe' => [
-        'model'  => App\User::class,
-        'key' => '',
-        'secret' => '',
-    ],
-
     'facebook' => [
         'client_id' => env('FB_CLIENT_ID'),
         'client_secret' => env('FB_CLIENT_SECRET'),
@@ -46,5 +25,34 @@ return [
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URL'),
     ],
-
+    'darksky' => [
+        'key'     => env('DARK_SKY_API_KEY'),
+        'baseUri' => 'https://api.darksky.net',
+    ],
+    'openweathermap' => [
+        'key'     => env('OPENWEATHERMAP_API_KEY'),
+        'baseUri' => 'http://api.openweathermap.org',
+    ],
+    'mapquest' => [
+        'key'     => env('MAPQUEST_API_KEY'),
+        'baseUri' => 'http://open.mapquestapi.com',
+    ],
+    'yelp' => [
+        'client_secret' => env('YELP_CLIENT_SECRET'),
+        'client_id'     => env('YELP_CLIENT_ID'),
+    ],
+    'ticketmaster' => [
+        'apikey'   => env('TICKETMASTER_APIKEY'),
+        'secret'   => env('TICKETMASTER_SECRET'),
+        'user'     => env('TICKETMASTER_USER'),
+        'password' => env('TICKETMASTER_PASSWORD'),
+    ],
+    'places' => [
+        'key'     => env('GOOGLE_PLACES_API_KEY'),
+        'baseUri' => 'https://maps.googleapis.com/maps/api/place/'
+    ],
+    'seatgeek' => [
+        'client_id' => env('SEATGEEK_CLIENT_ID'),
+        'secret'    => env('SEATGEEK_SECRET'),
+    ],
 ];
--- backend/config/yelp-api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'client_secret' => '1A64VKs0G0OJXbaRzjzTmcGZF1Bf0WlSpqbszxfW1M8NdaMxfdCukGlC7CRLBwud',
-    'client_id' => '-uEckuxuWNlR4tDX39--Og'
-];
