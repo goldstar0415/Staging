3133ebda9d6449c7e9ea5ace67bbcea991a8a129
Ingvar <igor@wirnex.com>
2017-03-16 12:38:31 +0300

    fix builder, supervisorctl user, composer, pgsql

--- backend/entry_point.sh
+++ backend/entry_point.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-#chmod -R 777 /data/tmp/php && chown -R 80:80 /data/ && composer install && \
+chmod -R 777 /data/tmp/php && chown -R 80:80 /data/ && composer install && \
+# Put environment variables (including from Docker) in to php conf file to use in env()
 env | sed "s/\(.*\)=\(.*\)/env[\1]='\2'/" > /data/conf/php-fpm-www-docker-env.conf && \
 supervisorctl restart php-fpm && \
 /config/bootstrap.sh
