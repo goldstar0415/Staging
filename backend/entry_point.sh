34a3772129c32c3877176aba1c0183ce2f716d47
EgorVorozhtsov <egor@vorozhtsov.com.ru>
2017-03-16 12:09:25 +0300

    sleep 20s, .editorconfig

--- backend/entry_point.sh
+++ backend/entry_point.sh
@@ -2,5 +2,6 @@
 
 #chmod -R 777 /data/tmp/php && chown -R 80:80 /data/ && composer install && \
 env | sed "s/\(.*\)=\(.*\)/env[\1]='\2'/" > /data/conf/php-fpm-www-docker-env.conf && \
-supervisorctl restart php-fpm && \
-/config/bootstrap.sh
+    echo "Sleep 20s..." && sleep 20 && \
+    supervisorctl restart php-fpm && \
+    /config/bootstrap.sh
