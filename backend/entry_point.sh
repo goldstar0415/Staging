064bf27415037e15b9b2ee8f0012a8b76c3a6915
EgorVorozhtsov <egor@vorozhtsov.com.ru>
2017-03-21 16:36:25 +0300

    volume permissions for backend

--- backend/entry_point.sh
+++ backend/entry_point.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Project files are: " && ls -la
+
 set -e
 set -u
 
-mkdir -p /data/tmp/php && chmod -R 777 /data/tmp/php && chown -R 80:80 /data/
+mkdir -p /data/tmp/php && chmod -R 777 /data/tmp/php
+#&& chown -R 80:80 /data/
     # Put environment variables (including from Docker) in to php conf file to use in env()
 #    env | sed "s/\(.*\)=\(.*\)/env[\1]='\2'/" > /data/conf/php-fpm-www-docker-env.conf && \
 #    echo "Sleep 20s..." && sleep 20 && composer install && \
@@ -26,14 +29,16 @@ if [ "$(ls /config/init/)" ]; then
 fi
 
 # Put environment variables (including from Docker) in to php conf file to use in the env()
-env | sed "s/\(.*\)=\(.*\)/env[\1]='\2'/" | grep -v "''" > /data/conf/php-fpm-www-docker-env.conf && \
-    echo "Sleep 20s..." && sleep 20 && composer install
+env | sed "s/\(.*\)=\(.*\)/env[\1]='\2'/" | grep -v "''" > /data/conf/php-fpm-www-docker-env.conf
 
+chown www:www /var/www/zoomtivity-backend/storage -R && chmod 777 /var/www/zoomtivity-backend/storage -R
+
+echo "Sleep 20s..." && sleep 20 && composer install
 
 # We have TTY, so probably an interactive container...
 if test -t 0; then
   # Run supervisord detached...
-  supervisord $SUPERVISOR_PARAMS
+  supervisord ${SUPERVISOR_PARAMS}
 
   # Some command(s) has been passed to container? Execute them and exit.
   # No commands provided? Run bash.
@@ -50,5 +55,5 @@ else
   if [[ $@ ]]; then
     eval $@
   fi
-  supervisord -n $SUPERVISOR_PARAMS
+  supervisord -n ${SUPERVISOR_PARAMS}
 fi
