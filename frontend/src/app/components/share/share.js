cabbe27b6254e7d66cbd31d51d8e2ad1779b23c2
Евгений Бондарь <j.bondar@2-up.ru>
2017-03-31 19:40:15 +0300

    Share url fix 2

--- frontend/src/app/components/share/share.js
+++ frontend/src/app/components/share/share.js
@@ -74,7 +74,8 @@
       {
           vm.text = vm.item.title;
           vm.description = vm.item.description;
-          vm.url = $state.href('spot', {spot_id: vm.item.id, user_id: vm.item.user_id, spot_slug: vm.item.slug}, {absolute: true});
+          vm.url = $state.href('spot', {spot_id: vm.item.id, user_id: vm.item.user_id || 0, spot_slug: vm.item.slug}, {absolute: true});
+          console.log(vm.url);
           vm.picture = vm.item.cover_url.medium;
       }
 
