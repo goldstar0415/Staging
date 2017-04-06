4d0ab1ff72fc29395a812221bccd58c1b349e55c
EgorVorozhtsov <egor@vorozhtsov.com.ru>
2017-04-06 10:05:28 +0300

    a postgres backup script to work inside a docker container

--- backup.sh
+++ backup.sh
@@ -8,8 +8,9 @@ TARGET_SERVICE=database
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BACKUPS_PATH=${PROJECT_ROOT}/postgres/backups
 DUMP_FILE=${DOCKER_PROJECT}_`date +%Y-%m-%d"_"%H_%M_%S`
+CONTAINER_ID=$(docker ps --filter="name=${DOCKER_PROJECT}_${TARGET_SERVICE}" -q -n 1)
 
-docker-compose -p ${DOCKER_PROJECT} exec ${TARGET_SERVICE} \
+docker exec ${CONTAINER_ID} \
     pg_dumpall -c -U postgres > ${BACKUPS_PATH}/${DUMP_FILE}.sql
 
 tar -zcvf ${BACKUPS_PATH}/${DUMP_FILE}.tgz -C ${BACKUPS_PATH} ${DUMP_FILE}.sql
