31ac173928115795425e954cd77bd44f563ccf8f
goldstar0415 <topdeveloper1212@gmail.com>
2017-04-19 15:44:38 +0300

    ZOOM-313 reformat code

--- backend/app/Http/Controllers/PrerenderController.php
+++ backend/app/Http/Controllers/PrerenderController.php
@@ -1,9 +1,9 @@
 <?php namespace App\Http\Controllers;
 
-use Illuminate\Http\Response;
 use Illuminate\Http\Request;
-use Route;
+use Illuminate\Http\Response;
 use Log;
+use Route;
 
 /**
  * Class PrerenderController
@@ -13,38 +13,37 @@ use Log;
 class PrerenderController extends Controller
 {
 
-	public function __construct()
-	{
-		$this->middleware('auth', ['except' => ['render']]);
-	}
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['render']]);
+    }
 
-	public function render($page_url)
-	{
-		$route = self::resolve($page_url);
+    public function render($page_url)
+    {
+        $route = self::resolve($page_url);
 
-		if ( !$route )
-			return new Response('Not found', 404);
+        if (!$route)
+            return new Response('Not found', 404);
 
-		return Route::dispatch( Request::create($route, 'GET') );
-	}
+        return Route::dispatch(Request::create($route, 'GET'));
+    }
 
-	final protected static function resolve($url)
-	{
-		switch (true)
-		{
-			case preg_match('/^(\d+)\/spot\/(\d+)/i', $url, $params):
-				return sprintf('spots/%s/preview', $params[2]);
+    final protected static function resolve($url)
+    {
+        switch (true) {
+            case preg_match('/^(\d+)\/spot\/(\d+)/i', $url, $params):
+                return sprintf('spots/%s/preview', $params[2]);
 
-			case preg_match('/^(\d+)\/article\/([^\/]+)/i', $url, $params):
-				return sprintf('posts/%s/preview', strtolower( $params[2] ));
+            case preg_match('/^(\d+)\/article\/([^\/]+)/i', $url, $params):
+                return sprintf('posts/%s/preview', strtolower($params[2]));
 
             // use '[a-zA-Z0-9]+' instead of '\d+' for hash
             case preg_match('/^areas\/(\d+)/i', $url, $params):
-				return sprintf('areas/%s/preview', strtolower( $params[1] ));
+                return sprintf('areas/%s/preview', strtolower($params[1]));
 
-			default:
-				return null;
-		}
-	}
+            default:
+                return null;
+        }
+    }
 
 }
