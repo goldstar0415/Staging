77cad6b7a61fb64b8b880dd50d90c5bf80c75022
goldstar0415 <topdeveloper1212@gmail.com>
2017-05-11 11:03:09 +0300

    ZOOM-347 rename pass reset email template in order to make laravel's renderer work

--- backend/config/auth.php
+++ backend/config/auth.php
@@ -59,7 +59,7 @@ return [
     */
 
     'password' => [
-        'email' => 'emails.password',
+        'email' => 'emails.password-reset',
         'table' => 'password_resets',
         'expire' => 60,
     ],
